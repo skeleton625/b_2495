e0b50487bc172db2296f647712c9619150236c0b
skeleton625 <chaos0824@naver.com>
2018-08-04 20:36:10 +0900

    2495 문제 풀이 완료

--- baekjoon_2495/main.cpp
+++ baekjoon_2495/main.cpp
@@ -3,6 +3,22 @@ using namespace std;
 
 int main()
 {
-
+	int c, max;
+	char i1[9], n;
+	for (int i = 0; i < 3; i++) {
+		c = 1;
+		max = -1;
+		cin >> i1;
+		n = i1[0];
+		for (int j = 1; j < 8; j++) {
+			if (n == i1[j]) c++;
+			else {
+				n = i1[j];
+				c = 1;
+			}
+			if (max < c) max = c;
+		}
+		cout << max << endl;
+	}
 	return 0;
 }
